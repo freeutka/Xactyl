9931a839f513a38df97a599bda7a597e5a5b512b
freeutka <freeutka@inbox.lv>
2025-10-26 13:09:04 +0000

    Pterodactyl --> Xactyl

--- app/Notifications/MailTested.php
+++ app/Notifications/MailTested.php
@@ -20,8 +20,8 @@ class MailTested extends Notification
     public function toMail(): MailMessage
     {
         return (new MailMessage())
-            ->subject('Pterodactyl Test Message')
+            ->subject('Xactyl Test Message')
             ->greeting('Hello ' . $this->user->name . '!')
-            ->line('This is a test of the Pterodactyl mail system. You\'re good to go!');
+            ->line('This is a test of the Xactyl mail system. You\'re good to go!');
     }
 }
