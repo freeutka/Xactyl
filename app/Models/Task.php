1214d2d43a236694787dfa8b01b591a4e4c85271
freeutka <freeutka@inbox.lv>
2025-10-27 17:29:02 +0000

    More rebase

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -33,7 +33,7 @@ class Task extends Model
     public const RESOURCE_NAME = 'schedule_task';
 
     /**
-     * The default actions that can exist for a task in Pterodactyl.
+     * The default actions that can exist for a task in Xactyl.
      */
     public const ACTION_POWER = 'power';
     public const ACTION_COMMAND = 'command';
